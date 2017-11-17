836a188df9c4c822bf5f6e51e8ebb0bf500aa723
root <root@vm99370.local>
2017-11-17 18:00:00 +0300

    fixed telegram request repeat when response in delayed due to a lot of coords

--- src/Telegram/TelegramChatHandler.php
+++ src/Telegram/TelegramChatHandler.php
@@ -27,6 +27,13 @@ class TelegramChatHandler {
 	}
 	
 	public function handleInput() {
+		
+		http_response_code(200);
+		
+		if (is_callable('fastcgi_finish_request')) {
+			fastcgi_finish_request();
+		}
+		
 		$input = TelegramBot\Request::getInput();
 		if($input == null || strlen($input) == 0) { 
 			return; 
@@ -40,7 +47,16 @@ class TelegramChatHandler {
 		$messageId = $this->input->message->message_id;
 
 		//$this->logInput();
-
+		
+		$geolocation = new Geolocation\GeolocationHandler($message);
+		
+		$cords = $geolocation->coordinates;
+		if (!empty($cords)) {
+			$this->handleCoords($cords, $messageId, $message, $geolocation);
+		}
+	}
+	
+	private function handleCoords($cords, $messageId, $message, $geolocation) {
 		$lang = substr($this->input->message->from->language_code, 0, 2);
 		$dict = [
 			'en' => [
@@ -53,47 +69,42 @@ class TelegramChatHandler {
 			]
 		];
 		
-		$geolocation = new Geolocation\GeolocationHandler($message);
+		$count = count($cords);
+		$hasReplied = false;
 		
-		$cords = $geolocation->coordinates;
-		if (!empty($cords)) {
-			$count = count($cords);
-			$hasReplied = false;
+		if ($count > 1) {
+			$this->sendMessage($count.' '.$dict[$lang]['coordsFound'], true, $messageId);
+			$hasReplied = true;
+		}
+		
+		for($i=0; $i<$count; $i++) {
+			$c = $cords[$i];
 			
-			if ($count > 1) {
-				$this->sendMessage($count.' '.$dict[$lang]['coordsFound'], true, $messageId);
-				$hasReplied = true;
-			}
-			
-			for($i=0; $i<$count; $i++) {
-				$c = $cords[$i];
-				
-				if ($count == 1 && $c->string != trim($message)) {
-					$replyToId = !$hasReplied ? $messageId : null;
-					$this->sendMessage($c->string, true, $messageId, $replyToId);
-					if (!is_null($replyToId)) { $hasReplied = true; }
-				}
-				
-				$urls = $geolocation->mapUrls($c->lat, $c->lon);
-				if(is_array($urls) && count($urls) > 0) {
-					$replyToId = !$hasReplied ? $messageId : null;
-					$text = ($count > 1 ? $dict[$lang]['coordNumber'].($i+1).': ' : '').join(' | ', $urls);
-					$this->sendMessage($text, true, $replyToId);
-					if (!is_null($replyToId)) { $hasReplied = true; }
-				}
-				
-				
-				$address = $geolocation->reverseGeocode($c->lat, $c->lon, $lang);
-				if (!empty($address)) {
-					$replyToId = !$hasReplied ? $messageId : null;
-					$this->sendMessage($address, true, $replyToId);
-					if (!is_null($replyToId)) { $hasReplied = true; }
-				}
-				
+			if ($count == 1 && $c->string != trim($message)) {
 				$replyToId = !$hasReplied ? $messageId : null;
-				$this->sendLocation($c->lat, $c->lon, true, $replyToId);
+				$this->sendMessage($c->string, true, $messageId, $replyToId);
 				if (!is_null($replyToId)) { $hasReplied = true; }
 			}
+			
+			$urls = $geolocation->mapUrls($c->lat, $c->lon);
+			if(is_array($urls) && count($urls) > 0) {
+				$replyToId = !$hasReplied ? $messageId : null;
+				$text = ($count > 1 ? $dict[$lang]['coordNumber'].($i+1).': ' : '').join(' | ', $urls);
+				$this->sendMessage($text, true, $replyToId);
+				if (!is_null($replyToId)) { $hasReplied = true; }
+			}
+			
+			
+			$address = $geolocation->reverseGeocode($c->lat, $c->lon, $lang);
+			if (!empty($address)) {
+				$replyToId = !$hasReplied ? $messageId : null;
+				$this->sendMessage($address, true, $replyToId);
+				if (!is_null($replyToId)) { $hasReplied = true; }
+			}
+			
+			$replyToId = !$hasReplied ? $messageId : null;
+			$this->sendLocation($c->lat, $c->lon, true, $replyToId);
+			if (!is_null($replyToId)) { $hasReplied = true; }
 		}
 	}
 	
